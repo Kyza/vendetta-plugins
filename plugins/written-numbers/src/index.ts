b9a3b90faa41310322f48e32ec90778943704a3d
Kyza <kyza@kyza.net>
2023-03-22 04:44:08 -0400

    fix: remove /reload command

--- plugins/written-numbers/src/index.ts
+++ plugins/written-numbers/src/index.ts
@@ -1,10 +1,6 @@
 import { storage } from "@vendetta/plugin";
 import Settings from "./Settings";
 import {
-	register as register_reload,
-	unregister as unregister_reload,
-} from "./commands/reload";
-import {
 	register as register_writtenNumber,
 	unregister as unregister_writtenNumber,
 } from "./commands/writtenNumber";
@@ -21,12 +17,10 @@ storage.replace ??= {
 export default {
 	onLoad: () => {
 		register_writtenNumber();
-		register_reload();
 		patch_sendMessage();
 	},
 	onUnload: () => {
 		unregister_writtenNumber();
-		unregister_reload();
 		unpatch_sendMessage();
 	},
 	settings: Settings,
